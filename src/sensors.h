0fcf3fbe57248bd2e6c78ff7241a4c754f93bb7e
Calico <calicorrandall@gmail.com>
2025-04-15 14:31:59 -0700

    Working & cleanup
    Working code for two tested sensors working together, seemingly simultaneously.
    Cleanup code that was testing or old, adding comments.
    
    NEXT:
    Chain more than two ToF. Add another and test, then add another and test until have 10 working together.
    Create the circuit board.

--- src/sensors.h
+++ src/sensors.h
@@ -8,30 +8,18 @@
 
 #define DEV_I2C Wire
 
-#define LEDPIN 13
-
 #define TOTAL_TOFS 2
 
 #define XSHUT_PIN_1 A1
 #define XSHUT_PIN_2 A2
 
-// Addresses tested = 0x04 - 0x13
-#define I2C_ADDR_1 0x04
-#define I2C_ADDR_2 0x05
-
 // Time of Flight VL53L4CX sensor declarations.
-//VL53L4CX tof1(&DEV_I2C, XSHUT_PIN_1);
-//VL53L4CX tof2(&DEV_I2C, XSHUT_PIN_2);
+VL53L4CX tofs[TOTAL_TOFS] = { VL53L4CX(&DEV_I2C, XSHUT_PIN_1), VL53L4CX(&DEV_I2C, XSHUT_PIN_2) };
 
-VL53L4CX tofs[TOTAL_TOFS];
-unsigned char I2C_ADDRESSES[] = { 0x04, 0x02 };
-unsigned char XSHUT_PINS[] = { A1, A2 };
+// Addresses need to be even numbers or odd numbers (not consecutive)
+unsigned char I2C_ADDRESSES[] = { 0x02, 0x04 };
 
 void setupSensors() {
-    // Initialize serial for output.
-    Serial.begin(115200);
-    Serial.println("Beginning setup...");
-
     // Initialize I2C bus.
     DEV_I2C.begin();
     Serial.println("I2C bus initialized.");
@@ -39,9 +27,9 @@ void setupSensors() {
     // Configure VL53L4CX satellite component.
     // Sets the shut pin as OUTPUT, then writes it to LOW
     for(int i = 0; i < TOTAL_TOFS; i++) {
-        tofs[i] = VL53L4CX(&DEV_I2C, XSHUT_PINS[i]);
         tofs[i].begin();
     }
+    delay(100);
     for(int i = 0; i < TOTAL_TOFS; i++) {
         tofs[i].VL53L4CX_Off();
         tofs[i].InitSensor(I2C_ADDRESSES[i]);
@@ -49,11 +37,13 @@ void setupSensors() {
         Serial.print(i+1);
         Serial.println(" initialized.");
     }
+    delay(100);
     for(int i = 0; i < TOTAL_TOFS; i++) {
         tofs[i].VL53L4CX_StartMeasurement();
         Serial.print("Measurements started for ToF ");
         Serial.println(i+1);
     }
+    delay(100);
     //tof1.begin();
     //tof2.begin();
 
